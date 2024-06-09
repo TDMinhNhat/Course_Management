ae854734481457871790d2ff84b068599cc1c12a
MinhNhat <tdminhnhat13092003@gmail.com>
2024-06-10 00:40:42 +0700

    Done send mail reset password

--- CourseManagement_WebAPI/Controllers/PersonController.cs
+++ CourseManagement_WebAPI/Controllers/PersonController.cs
@@ -9,8 +9,11 @@ using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Mail;
+using System.Text;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using System.Data.Entity.Migrations;
 
 namespace CourseManagement_WebAPI.Controllers
 {
@@ -142,6 +145,39 @@ namespace CourseManagement_WebAPI.Controllers
                     DateModifier = target.DateModifier
                 };
                     return Request.CreateResponse(HttpStatusCode.OK, dto);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/Person/send-new-password")]
+        public HttpResponseMessage SolveForgotPassword([FromUri] string email)
+        {
+            using(CourseManagementEntities entities = new CourseManagementEntities())
+            {
+                Person result = entities.People.FirstOrDefault(p => p.Email.Equals(email));
+                if (result is null)
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Can't find the email = " + email);
+
+                int newPassword = new Random().Next(10000, 99999);
+                result.Password = newPassword.ToString();
+                entities.People.AddOrUpdate(result);
+                entities.SaveChanges();
+
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+                smtpClient.Credentials = new System.Net.NetworkCredential("jimtruong13092003@gmail.com", "qpvk xqqz ueza ylsi");
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.EnableSsl = true;
+
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress("jimtruong13092003@gmail.com", "Reset Password", System.Text.Encoding.UTF8);
+                message.To.Add(new MailAddress(email));
+                message.CC.Add("jimtruong13092003@gmail.com");
+                message.Subject = "Cài đặt lại mật khẩu";
+                message.Body = "Mật khẩu của bạn đã được thay đổi hành: " + newPassword;
+                message.BodyEncoding = Encoding.UTF8;
+                smtpClient.Send(message);
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Reset password successfully!");
             }
         }
     }
